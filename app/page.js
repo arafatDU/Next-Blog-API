279d5f5d0a726aac51c47b7326f5ff8fb50cb810
ARAFAT <108474267+arafatDU@users.noreply.github.com>
2024-09-08 02:13:55 +0600

    change home as client component

--- app/page.js
+++ app/page.js
@@ -1,11 +1,29 @@
 // app/page.js
+'use client';
+import { useContext } from 'react';
+import { AuthContext } from '../context/AuthContext';
+import Link from 'next/link';
+
 export default function HomePage() {
+  const { user } = useContext(AuthContext);
+
   return (
     <section>
-      <h1>Welcome to My Blog</h1>
-      <p>
-        Please <a href="/signup">sign up</a> or <a href="/login">log in</a> to access your dashboard.
-      </p>
+      {user ? (
+        // UI when the user is logged in
+        <>
+          <h1>Welcome Back, {user.username || 'User'}!</h1>
+          <p>Go to your <Link href="/dashboard">Dashboard</Link> to manage your blogs and categories.</p>
+        </>
+      ) : (
+        // UI when the user is not logged in
+        <>
+          <h1>Welcome to My Blog</h1>
+          <p>
+            Please <Link href="/signup">sign up</Link> or <Link href="/login">log in</Link> to access your dashboard.
+          </p>
+        </>
+      )}
     </section>
   );
 }
